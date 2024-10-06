81eb3879cbe27c8bf5ec3dc9272394bb41837045
cykim8811 <cykim8811@snu.ac.kr>
2024-10-06 20:51:10 +0000

    feat: responsive

--- frontend/src/views/MainView.tsx
+++ frontend/src/views/MainView.tsx
@@ -20,7 +20,7 @@ const MainView: React.FC = () => {
         setTimeout(() => {setTutortext("tutor.");}, 5500);
     }, []);
     return (
-        <div className="flex justify-center items-center h-screen text-7xl flex-col" style={{ fontFamily: "Noto Serif"}}>
+        <div className="flex justify-center items-center h-screen text-5xl md:text-7xl lg:text-8xl flex-col" style={{ fontFamily: "Noto Serif"}}>
             <motion.span 
                 className="flex flex-row items-center italic"
                 initial={{ opacity: 0, y: 50 }}
@@ -36,13 +36,13 @@ const MainView: React.FC = () => {
                 <span style={{fontSize: "1.05em", marginBottom: "0.4rem"}}>ephyr.</span>
             </motion.span>
             <motion.div 
-                className="text-2xl mt-20 mb-8"
+                className="text-lg md:text-2xl lg:text-3xl mt-20 mb-8"
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.8, delay: 0.3, ease: "easeOut" }}
             >
                 Your personal real-time AI math <span className="text-transparent">tuter.</span>
-                <div className="absolute right-0 top-0 text-2xl text-black">
+                <div className="absolute right-0 top-0 text-lg md:text-2xl lg:text-3xl text-black">
                 <div className="absolute">{tutortext}</div>
                     <motion.div
                         className="relative"
@@ -51,24 +51,52 @@ const MainView: React.FC = () => {
                         transition={{ duration: 0.2, delay: 5.8 }}
                     >
                         <motion.div
-                            className="decoration-wavy underline decoration-red-600 overflow-hidden text-transparent"
+                            className="decoration-wavy underline decoration-red-600 overflow-hidden text-transparent h-16"
                             initial={{ width: 0 }}
                             animate={{ width: "100%" }}
                             transition={{ duration: 0.5, delay: 2.3 }}
                         >
                             tutor.
                         </motion.div>
-                        <motion.div
-                            className="absolute top-8 -left-14 text-red-600 text-xl overflow-hidden fit-content"
-                            style={{ fontFamily: "Kalam" }}
-                            initial={{ maxWidth: 0 }}
-                            animate={{ maxWidth: "400%" }}
-                            transition={{ duration: 1.2, delay: 2.8 }}
-                        >
-                            <div className="whitespace-nowrap max-content block">
-                                Check the spelling of 'tuter'.
-                            </div>
-                        </motion.div>
+                        {
+                            window.innerWidth > 768 ?
+                            <motion.div
+                                className="absolute top-8 -left-20 md:-left-14 text-red-600 text-lg lg:text-2xl overflow-hidden fit-content"
+                                style={{ fontFamily: "Kalam" }}
+                                initial={{ maxWidth: 0 }}
+                                animate={{ maxWidth: "400%" }}
+                                transition={{ duration: 1.2, delay: 2.8 }}
+                            >
+                                <div className="whitespace-nowrap max-content block">
+                                    Check the spelling of 'tuter'.
+                                </div>
+                            </motion.div>
+                            :
+                            <span>
+                                <motion.div
+                                    className="absolute top-8 -left-20 md:-left-14 text-red-600 text-lg overflow-hidden fit-content"
+                                    style={{ fontFamily: "Kalam" }}
+                                    initial={{ maxWidth: 0 }}
+                                    animate={{ maxWidth: "400%" }}
+                                    transition={{ duration: 0.8, delay: 2.8 }}
+                                >
+                                    <div className="whitespace-nowrap max-content block">
+                                        Check the spelling
+                                    </div>
+                                </motion.div>
+                                <motion.div
+                                    className="absolute top-14 -left-2 md:-left-14 text-red-600 text-lg overflow-hidden fit-content"
+                                    style={{ fontFamily: "Kalam" }}
+                                    initial={{ maxWidth: 0 }}
+                                    animate={{ maxWidth: "400%" }}
+                                    transition={{ duration: 0.6, delay: 3.3 }}
+                                >
+                                    <div className="whitespace-nowrap max-content block">
+                                        of 'tuter'.
+                                    </div>
+                                </motion.div>
+                            </span>
+                        }
                         <span className="text-transparent">tutor.</span>
                     </motion.div>
                 </div>
@@ -79,7 +107,7 @@ const MainView: React.FC = () => {
                 animate={{ opacity: 1 }}
                 transition={{ duration: 0.8, delay: 1, ease: "easeOut" }}
             >
-                <Button>
+                <Button className="mb-24">
                     Get Started
                     <ArrowRight size={16} />
                 </Button>
